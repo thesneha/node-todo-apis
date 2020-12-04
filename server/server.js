fc4324941e796385333d38d288bd0d7472a8c8f4
sneha <sssnehatiwaari0@gmail.com>
2020-12-05 01:55:57 +0530

    added /todo/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express=require('express');
 var bodyParser=require('body-parser');
+const {ObjectID}=require('mongodb');
 
 var {mongoose} =require('./db/mongoose');
 var {Todo} =require('./models/todo');
@@ -36,6 +37,30 @@ app.get('/todos',(req,res)=>{
     res.status(400).send(err);
   });
 });
+
+app.get('/todos/:id',(req,res)=>{
+  //res.send(req.params.id);
+
+  var id=req.params.id;
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo)=>{
+    if (!todo) {
+      //return console.log('user not found');
+      res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((err)=>{
+    res.status(404).send();
+  });
+
+});
+
+
+
+
 
 
 
