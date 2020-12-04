5f013cc5a48816c39079bf416998c9106392b65d
sneha <sssnehatiwaari0@gmail.com>
2020-12-04 10:51:12 +0530

    added /todo route and refactor mongoose

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -7,7 +7,7 @@ MongoClient.connect('mongodb://localhost:27017/todoApp',(err,client)=>{
   }
   console.log('connected to mongodb server');
   const db=client.db('todoApp');
-  db.collection('todos').findOneAndUpdate({
+  db.collection('todos').findOneAndUpdate ({
     _id:new ObjectID('5fc6f83e54687087ff9cb1af')
   },{
     $set:{
---
+++ server/server.js
@@ -0,0 +1,55 @@
+var express=require('express');
+var bodyParser=require('body-parser');
+
+var {mongoose} =require('./db/mongoose');
+var {Todo} =require('./models/todo');
+var {User} =require('./models/user');
+
+var app=express();
+
+app.listen(3000,()=>{
+  console.log("server started on port 3000");
+})
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=>{
+  //console.log(req.body);
+  var todo=new Todo({
+    text:req.body.text
+  })
+
+  todo.save().then((doc)=>{
+    res.send(doc);
+  }).catch((err)=>{
+    res.status(400).send(err);
+  });
+
+});
+
+
+
+// var newTodo= new Todo({
+//   text:'heloo this is sneha  collection'
+// });
+// var newTodo=new Todo({
+//   text:'              xydz'
+// });
+//
+// newTodo.save().then((doc)=>{
+//   console.log( doc );
+// }).catch((e)=>{
+//   console.log('nable to save doc', e );
+// });
+
+
+
+// var newUser=new User({
+//   email:'sssnehatiwari0@gmail.com'
+// })
+//
+// newUser.save().then((doc)=>{
+//   console.log( doc );
+// }).catch((e)=>{
+//   console.log('nable to save doc', e );
+// });
